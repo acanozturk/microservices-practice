6f465445a2d75ecc8737cef252899c1786bd7ae8
Ali Can Öztürk <acanozturk96@gmail.com>
2021-06-25 12:29:10 +0300

    added multiple environments

--- currency-exchange-service/src/main/java/com/practice/microservices/currencyexchangeservice/data/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/practice/microservices/currencyexchangeservice/data/CurrencyExchange.java
@@ -4,15 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CurrencyExchange {
 
-    private Long id;
+    private long id;
     private String from;
     private String to;
-    private BigDecimal conversionRate;
+    private double conversionRate;
+    private String environment;
 }
--- currency-exchange-service/src/main/java/com/practice/microservices/currencyexchangeservice/services/implementations/CurrencyExchangeServiceImpl.java
+++ currency-exchange-service/src/main/java/com/practice/microservices/currencyexchangeservice/services/implementations/CurrencyExchangeServiceImpl.java
@@ -2,24 +2,43 @@ package com.practice.microservices.currencyexchangeservice.services.implementati
 
 import com.practice.microservices.currencyexchangeservice.data.CurrencyExchange;
 import com.practice.microservices.currencyexchangeservice.services.interfaces.CurrencyExchangeService;
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
+@AllArgsConstructor
 public class CurrencyExchangeServiceImpl implements CurrencyExchangeService {
+
+    private final Environment environment;
 
     @Override
     public CurrencyExchange getConversionRate(final String from, final String to) {
         final CurrencyExchange exchange = new CurrencyExchange();
 
-        exchange.setId(ThreadLocalRandom.current().nextLong(1, 10000));
+        exchange.setId(getRandomId());
         exchange.setFrom(from);
         exchange.setTo(to);
-        exchange.setConversionRate(BigDecimal.valueOf(8.67));
+        exchange.setConversionRate(getRandomConversionRate());
+        exchange.setEnvironment(getCurrentEnvironment());
 
         return exchange;
     }
 
+    private long getRandomId() {
+
+        return ThreadLocalRandom.current().nextLong(1, 10000);
+    }
+
+    private double getRandomConversionRate() {
+
+        return ThreadLocalRandom.current().nextDouble(1, 10);
+    }
+
+    private String getCurrentEnvironment() {
+
+        return environment.getProperty("local.server.port");
+    }
 }
